c52138cfc8f3558e0491bbb2d51c46a1678cc041
Gaurabag <146935663+Gaurabag@users.noreply.github.com>
2024-11-11 13:21:30 +0530

    Update constants.js
    Signed-off-by: Gaurabag <146935663+Gaurabag@users.noreply.github.com>

--- browser/flagr-ui/src/constants.js
+++ browser/flagr-ui/src/constants.js
@@ -27,26 +27,6 @@ const ENVURLS = {
   }
 }
 
-const ENVS = {
-  PROD: 'PROD',
-  STAGE: 'STAGE',
-  DEV: 'DEV'
-}
-
-const ENVURLS = {
-  PROD: {
-    VUE_APP_API_URL : 'http://flagr-new.allen-live.in/api/v1',
-    VUE_APP_SSO_API_URL : 'https://api.allen-live.in/internal-bff/',
-  },
-  STAGE: {
-    VUE_APP_API_URL : 'https://flagr-new.allen-stage.in/api/v1',
-    VUE_APP_SSO_API_URL : 'https://bff.allen-stage.in/internal-bff/',
-  },
-  DEV: {
-    VUE_APP_API_URL : 'http://flagr-new.allen-demo.in/api/v1',
-    VUE_APP_SSO_API_URL : 'https://bff-dev.allen-demo.in/internal-bff/',
-  }
-}
 export default {
   API_URL,
   SSO_URL,
