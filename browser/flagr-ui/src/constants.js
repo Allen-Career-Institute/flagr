e21b50e73c0b03ff6745c46d036031f17d6ee6fb
Gaurabag <Gaurab.Agarwala@allen.in>
2024-12-20 16:01:13 +0530

    fix: use https

--- browser/flagr-ui/src/constants.js
+++ browser/flagr-ui/src/constants.js
@@ -30,7 +30,7 @@ const ENVS = {
 
 const ENVURLS = {
   PROD: {
-    VUE_APP_API_URL : 'http://flagr-new.allen-live.in/api/v1',
+    VUE_APP_API_URL : 'https://flagr-new.allen-live.in/api/v1',
     VUE_APP_SSO_API_URL : 'https://api.allen-live.in/internal-bff/',
   },
   STAGE: {
